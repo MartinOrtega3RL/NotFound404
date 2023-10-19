dce47a963a3da4877a98dcc769586c972a8c48b5
MartinOrtega3RL <martinarto2015@gmail.com>
2023-10-19 13:52:19 -0300

    Modulo 404

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const sports = ['Futbol', 'Tenis', 'Basquet',"Voley","Rugby"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const changeSport = () => {
+    setCurrentIndex((currentIndex + 1) % sports.length);
+  };
+
+
+useEffect(() => {
+    const interval = setTimeout(changeSport, 2300); // Cambia cada 2 segundos
+
+    return () => clearTimeout(interval);
+  }, [currentIndex]);
+
+  const currentSport = sports[currentIndex];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="not-found">
+      <div className='not-found-content'>
+        <div className="ball-container">
+          <div className={`ball ${currentSport.toLowerCase()}`}></div>
+        </div>
+        <div className="error-code">
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+        <h1>¡Oops! Página no encontrada</h1>
+        <p>Lo sentimos, la página que estás buscando no existe.</p>
+        <p>¿Quieres volver a la página de inicio?</p>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
+
